1d21abab641b658601dff00202b4dab7d72cb10b
“ML-WSB” <wrx81885@student.wroclaw.merito.pl>
2024-03-24 22:30:19 +0100

    Dodano komendę lint do Makefile

--- app.py
+++ app.py
@@ -1,2 +1,21 @@
-print("Witaj w cw_praca_z_kodem!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Witaj w cw_praca_z_kodem!'
+
+@app.route('/hello')
+def hello():
+    # Zamiast "Hello, World!", podaj swoje imię
+    return 'Witaj, Mateusz!'
+
+@app.route('/hello/<name>')
+def hello_name(name):
+    # Ta trasa pozostaje bez zmian, nadal przyjmuje parametr 'name'
+    return 'Witaj, {}!'.format(name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
